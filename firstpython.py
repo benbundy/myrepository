bb80f21f6a62427dad1a152164fe748298a9adfd
benbundy <ben.bundy@arbor.edu>
2020-11-30 09:08:37 -0600

    Update firstpython.py
    Added a plot() function to the end of my Yahtzee function

--- firstpython.py
+++ firstpython.py
@@ -1,7 +1,9 @@
 # This is my new Python file I guess
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def Yahtzee():
   roll = np.random.randint(1,7,(1,5))
   return roll
+  plt.plot(roll, range(1,6))
